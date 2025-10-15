1594aaf5bd28ad9a04de2b1514453c402b3bbc04
zaratounga <bertrand@jesenberger.fr>
2025-10-15 17:54:49 +0200

    themes : wip

--- theme.asm
+++ theme.asm
@@ -56,9 +56,8 @@ end:
     rts
 
 not_found:
-    mov r0,msg_not_found
-    clc
-    swi error
+    sec
+    swi error,msg_not_found
     rts
 
 help:
@@ -79,18 +78,51 @@ msg_help:
     .byte 0
 
 themes:
-    pstring("normal")
-    pstring("ocean")
-    pstring("retro")
-    pstring("neon")
-    pstring("forest")
-    pstring("mono")
+    pstring("classic")
+    pstring("solar")
+    pstring("monokai")
+    pstring("xterm")
+    pstring("zenburn")
+    pstring("paper")
+    pstring("green")
+    pstring("amber")
+    pstring("dracula")
+    pstring("gruvbox")
+    pstring("dark")
+    pstring("nord")
+    pstring("one-dark")
+    pstring("tokyo")
+    pstring("cattppuccin")
+    pstring("everforest")
+    pstring("rosepine")
+    pstring("kanagawa")
+    pstring("c128")
+    pstring("c16")
+    pstring("plus4")
+    pstring("sx64")
     .byte 0
+
 themes_colors:
-    .word $71E6,$3fe6
-    .word $6e13,$7cfa
-    .word $4b07,$1f3c
-    .word $067d,$a28f
-    .word $5c3f,$d71a
-    .word $0b1f,$c37e
+    .word $71E6,$3F5E  // Classic        - blue bg, white text, light-blue accents
+    .word $E100,$3D5C  // Solarized      - dark-blue bg, light-grey text, cyan/yellow subs
+    .word $7A00,$2F4E  // Monokai        - black bg, light-orange text, green accent
+    .word $1500,$3C7E  // XTerm          - black bg, light-grey text, cyan accent
+    .word $9C00,$4D5E  // Zenburn        - olive bg, grey text, pale-green titles
+    .word $20F1,$044D  // Paper Light    - light-grey bg, black text, blue title, grey accents
+    .word $3500,$2D5E  // Retro Green    - black bg, green text, cyan titles
+    .word $A800,$4C7E  // Amber          - black bg, orange text, yellow accents
+    .word $E400,$2F5C  // Dracula        - black bg, magenta text, cyan/yellow subs
+    .word $9C00,$4A7E  // Gruvbox        - black bg, beige text, orange accent
+    .word $C100,$3D7E  // Dark           - black bg, white text, cyan accents
+    .word $B1E6,$4F5C  // Nord           - dark-blue bg, snow white, frost blue accents
+    .word $7100,$2F5E  // One Dark       - very dark blue bg, grey text, blue accents
+    .word $4100,$2F6E  // Tokyo Night    - deep blue bg, light-blue text, purple accents
+    .word $E1F6,$7F4C  // Catppuccin     - mauve bg, text white, peach accents
+    .word $5100,$3D5E  // Everforest     - dark-green bg, light-grey text, green accents
+    .word $E1B6,$7F4C  // Rose Pine      - purple bg, rose text, gold accents
+    .word $A100,$3F4E  // Kanagawa       - dark bg, peach text, dragon blue 
+    .word $D5B0,$3C7E  // Boot128: fond gris foncé, texte vert clair, accent vert, bord=fond
+    .word $15F0,$3C7E  // BootC16: fond gris clair, texte noir, accent blanc, bord=fond
+    .word $71F0,$3C7E  // Plus4: fond blanc, texte noir, accent jaune, bord=fond
+    .word $36F1,$3C7E  // SX64: fond blanc, texte bleu, accent cyan, bord=fond
 }
