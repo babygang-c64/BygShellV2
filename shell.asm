eea54fe7dd020531ca3b1d2a3ce11138b1f8d4f2
zaratounga <bertrand@jesenberger.fr>
2025-10-19 20:37:44 +0200

    shell : suppressed env -> sys

--- shell.asm
+++ shell.asm
@@ -523,13 +523,7 @@ do_env:
 {
     .label OPT_D=1
     .label OPT_P=2
-    .label OPT_Q=4
 
-    ldy #0
-    sty options_params
-    lda nb_params
-    beq no_params
-    
     sec
     swi param_init,buffer,options_env
 
@@ -567,127 +561,17 @@ no_path:
     swi str2int
     lda zr1l
     sta bin_device
-    
+
+options_env:
+    pstring("dp")
     
 no_params:
-    // Q = quiet mode
-    lda options_params
-    and #OPT_Q
-    jne end_env
-
-    //-- clipboard content
-    swi pprint,msg_clipboard    
-    mov r0,#clipboard
-    sec
-    jsr print_ram_or_none
-    
-    //-- last command
-    swi pprint,msg_cmd
-    clc
-    mov r0,#$c002
-    jsr print_ram_or_none
-
-    //-- sh$ string
-    swi pprint,msg_sh_string    
-    sec
-    swi get_basic_string, sh_string
-    cpx #0
-    bne is_sh_string
-    swi pprint_nl,msg_none
-    jmp not_sh_string
-    
-is_sh_string:
-    txa // length in A, R0 is string start (no length byte)
-    ldy #$ff // is incremented
-    ldx #0 // no screen to petscii conv
-    jsr pprint_ram.basic
-    jsr carriage_return
-
-    //-- sh% integer
-not_sh_string:
-    swi pprint,msg_sh_int
-    swi get_basic_int,var_int_sh_desc
-    jsr print_int16
-
-    //-- Current device
-    swi pprint,msg_device
-    lda CURRDEVICE
-    bne is_value_device
-    swi pprint_nl,msg_none
-    jmp next_bin_device
-
-is_value_device:
-    jsr print_int8
-
-    //-- bin device
-next_bin_device:
-    swi pprint,msg_bin
-    swi pprint,msg_device
-    mov r0,#bin_device
-    jsr bios.bios_ram_get_byte
-    cmp #0
-    bne is_value_bin_device
-    swi pprint_nl,msg_none
-    jmp next_bin_path
-    
-is_value_bin_device:
-    jsr print_int8
-
-    //-- bin path
-next_bin_path:
-    swi pprint,msg_bin
-    swi pprint,msg_path
-    mov r0,#bin_path
-    clc
-    jsr print_ram_or_none
-
-end_env:
     sec
     rts
-
-print_ram_or_none:
-    jsr pprint_ram
-    cmp #0
-    bne carriage_return
-    swi pprint,msg_none
 
 carriage_return:
     lda #13
     jmp CHROUT
-
-print_int8:
-    ldy #0
-    sty zr0h
-    sta zr0l
-print_int16:
-    ldx #%10011111
-    swi pprint_int
-    jmp carriage_return
-
-options_env:
-    pstring("dpq")
-sh_string:
-    .text "SH$"
-    .byte 0
-msg_clipboard:
-    pstring("Clip:")
-msg_sh_string:
-    pstring("SH$ :")
-msg_sh_int:
-    pstring("SH% :")
-msg_bin:
-    pstring("BIN ")
-msg_path:
-    pstring("Path:")
-msg_device:
-    pstring("Dev :")
-msg_cmd:
-    pstring("Cmd :")
-msg_none:
-    pstring("-")
-var_int_sh_desc:
-    .text "SH%"
-    .byte 0
 }
 
 //---------------------------------------------------------------
